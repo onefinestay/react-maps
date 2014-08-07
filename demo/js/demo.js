d3ddded4b91eb837de78ebae5c17e844402ab6e2
Andrew Ingram <andy@andrewingram.net>
2014-08-07 13:05:57 +0100

    Use Geolocation API in Demo

--- demo/js/demo.js
+++ demo/js/demo.js
@@ -6,9 +6,25 @@ var position = {
   lng: -0.1409180
 };
 
-React.renderComponent(
-  <ReactMaps.Map center={position} zoom={12} />,
-  document.getElementById('map')
-);
+if ("geolocation" in navigator) {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    var center = {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude
+    };
+    React.renderComponent(
+      <ReactMaps.Map center={center} zoom={12} />,
+      document.getElementById('map')
+    );
+
+  });
+} else {
+  React.renderComponent(
+    <ReactMaps.Map center={position} zoom={12} />,
+    document.getElementById('map')
+  );
+}
+
+
 
 window.React = React;
