cbde8e9b743554ce3018232f5219f965d09acff0
Andrew Ingram <andy@andrewingram.net>
2014-08-07 16:58:52 +0100

    remove geolocation for now

--- scripts/demo.jsx
+++ scripts/demo.jsx
@@ -8,21 +8,6 @@ var position = {
   lng: -0.1409180
 };
 
-if ("geolocation" in navigator) {
-  navigator.geolocation.getCurrentPosition(function(position) {
-    var center = {
-      lat: position.coords.latitude,
-      lng: position.coords.longitude
-    };
-    React.renderComponent(
-      <ReactMaps.Map center={center} zoom={12} />,
-      document.getElementById('map')
-    );
-
-  });
-} else {
-}
-
 React.renderComponent(
   <ReactMaps.Map center={position} zoom={12} />,
   document.getElementById('map')
